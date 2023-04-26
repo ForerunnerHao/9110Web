f70a3e18f5e557f969faf4022f32369ea4a3437b
HAOXIONG HU <huhaox@deakin.edu.au>
2023-04-27 00:37:16 +1000

    version_0.1

---
+++ js/changeLanguage.js
@@ -0,0 +1,43 @@
+var currentLanguage = 0;
+var nav_home = ['· 首页', '· Home', '· ホームページ'];
+var nav_products = ['· 产品','· Products', '`· 商品 '];
+var nav_bookings = ['· 预定','· Bookings', '·  予約'];
+var nav_gallery = ['· 画廊','· Gallery', '· ギャラリー'];
+var nav_contactus = ['· 联系我们','· Contact us', '· お問い合わせ'];
+var nav_language = ['· 语言','· Language', '· 言語'];
+
+var section_welcome_top = ['欢 迎 来 到','W E L C O M E', 'よ う こ そ'];
+var section_welcome_mid = ['D o g X e r c i s e','D o g X e r c i s e', 'D o g X e r c i s e'];
+var section_welcome_bot = [
+  '让您心爱的宠物拥有更健康的生活方式。我们拥有持牌训练员、高品质的设备以及宽敞的设施，为您的爱犬提供最好的训练服务。',
+  'Give your beloved pet a healthier lifestyle. We have licensed trainers, high-quality equipments, as well as spacious facilities to give the best training services to your dog.'
+, 'あなたの愛するペットがより健康的な生活を送るために。ライセンスを持つトレーナー、高品質な設備、広々とした施設を備え、あなたの愛犬に最高のトレーニングサービスを提供します。'
+];
+
+var section_trainer_top = ['专业认证的','P R O F I T I O N A L','専門的な認証'];
+var section_trainer_mid = ['汪汪训练师','T r a i n e r','わんわんトレーナー'];
+var section_trainer_bot = [
+  '我们的训练师都是经验丰富的专业人士，具备良好的动物行为学知识和技能，能够为您的宠物提供高水平的训练服务。'
+,'Our trainers are experienced professionals with good animal behavior knowledge and skills, able to provide high-level training services for your pets.'
+,'私たちのトレーナーは、経験豊富なプロフェッショナルであり、優れた動物行動学の知識と技術を持ち、ペットに高水準のトレーニングサービスを提供できます。'];
+
+var section_plan_top = ['个性化的','P E R S O N A L I Z E D','個別に合わせた'];
+var section_plan_mid = ['训练计划','Training Plan','トレーニングプログラム'];
+var section_plan_bot = [
+  '我们的训练师会根据您宠物的年龄、品种、体型、性格和健康状况等因素，为您的宠物制定个性化的训练计划，旨在为您的宠物提供最佳的锻炼效果。',
+  'Our trainers will develop a personalized training plan for your pet based on factors such as your pet\'s age, breed, size, personality and health, aiming to provide the best for your pet exercise effect.',
+  '私たちのトレーナーは、ペットの年齢、品種、体型、性格、健康状態などの要因に基づいて、ペットに合わせた個別のトレーニングプランを作成します。これにより、ペットに最適な運動効果を提供します。'];
+
+var section_cs_top = ['超高质量的','H I G H - Q U A L I T Y','超高品質の'];
+var section_cs_mid = ['客户服务','Customer Service','カスタマーサービス'];
+var section_cs_bot = [
+  '我们的客户服务团队提供高效友好的服务，随时解答问题并提供帮助，以确保您在使用我们的服务时有愉快的体验。',
+  'Our customer service team provides efficient and friendly service, answering questions and providing assistance at any time to ensure that you have a pleasant experience when using our services.',
+  '「私たちのカスタマーサポートチームは、効率的でフレンドリーなサービスを提供し、いつでも質問に答えて助けを提供します。これにより、私たちのサービスを使用する際に愉快な体験が得られるようにします。'];
+
+var section_time_top = ['灵活可调的','F L E X I B L E','柔軟で調整可能な'];
+var section_time_mid = ['时间选择','Time Options','時間選択'];
+var section_time_bot = [
+  '我们提供灵活的时间选择，以适应您和您宠物的日常安排，让您轻松规划。',
+  'We offer flexible time options to fit you and your pet\'s daily schedule, making it easy to plan your work.',
+  '私たちは柔軟な時間選択を提供し、あなたとペットの日常スケジュールに合わせて調整し、簡単に計画できるようにします。'];
